6f3bd51b5b27accee454ccd572e90ca6beb081d4
nrossol <nrossol@gmail.com>
2015-11-07 15:52:50 -0700

    Shurikens that move and explode

---
+++ Madjam2015/Assets/ShurikenBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShurikenBehaviour : MonoBehaviour {
+
+
+    public float speed = 2.0f;
+    public float lifeTime = 8.0f;
+
+    private Rigidbody shurikenRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        shurikenRigidBody = GetComponent<Rigidbody>();
+        shurikenRigidBody.velocity = transform.forward * speed;
+
+        Object.Destroy(this.gameObject, lifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //this.transform.Translate(0, 0, (speed * Time.deltaTime), Space.Self);
+    }
+}
---
+++ Madjam2015/Assets/SimpleMovementController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovementController : MonoBehaviour {
+
+    public float forwardMoveSpeed = 0.5f;//Value is in m/s
+    public float turnSpeed = 80.0f;//value is in degrees per second
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float forwardMoveAmount = Input.GetAxis("Vertical") * this.forwardMoveSpeed;
+        float angularTurnAmount = Input.GetAxis("Horizontal") * this.turnSpeed * Time.deltaTime;
+
+        this.transform.Translate(0, 0, forwardMoveAmount);
+        this.transform.Rotate(0, angularTurnAmount, 0);
+    }
+}

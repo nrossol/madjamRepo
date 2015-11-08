ad12bbbc08b18d5a3f2e3746d4915c834f591312
GluthB <gluthb@gmail.com>
2015-11-08 00:04:23 -0700

    GUI Work
    - Health and score now show health and score.

---
+++ Madjam2015/Assets/HealthScoreUpdater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class HealthScoreUpdater : MonoBehaviour
+{
+    public GameManager gameManager;
+    public PlayerHealth health;
+
+    public TextMesh scoreText;
+    public TextMesh healthText;
+
+    public void Start()
+    {
+        health.OnDamage += DamageHandler;
+    }
+
+    public void Update()
+    {
+        scoreText.text = "Score: " + gameManager.surivedTime;
+    }
+
+    public void DamageHandler()
+    {
+        healthText.text = "Health: " + health.Health;
+    }
+}
+

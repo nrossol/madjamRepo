ea4661430a6506dadfe53aa35acfce29a9ee1f27
nrossol <nrossol@gmail.com>
2015-11-07 17:15:15 -0700

    Shurikens now leave smoke trails and blow up

--- Madjam2015/Assets/ShurikenBehaviour.cs
+++ Madjam2015/Assets/ShurikenBehaviour.cs
@@ -8,8 +8,10 @@ public class ShurikenBehaviour : MonoBehaviour {
     public float lifeTime = 8.0f;
 
     public GameObject explosionPrefab;
+    public ParticleSystem smokeTrail;
 
     private Rigidbody shurikenRigidBody;
+    private float smokeLingerTime = 12.0f;//Max time smoke trail emitter should be allowed to stick around until being deleted
 
 	// Use this for initialization
 	void Start () {
@@ -32,6 +34,12 @@ public class ShurikenBehaviour : MonoBehaviour {
     void BlowUp()
     {
         Instantiate(explosionPrefab, this.transform.position, this.transform.rotation);
+        if (this.smokeTrail)
+        {
+            Object.Destroy(this.smokeTrail.gameObject, this.smokeLingerTime);//Eventually garbage collect the smoke trail particle system emitter after all the smoke is long gone
+            this.smokeTrail.Stop();//The missile is gone, so stop emitting smoke
+            this.smokeTrail.transform.parent = null;//Detach the smoke trail from its parent (should only have 1 parent) so that it won't immediately disappear
+        }
         Object.Destroy(this.gameObject);
 
     }
